eb3005abdf28ae5b996749380166045104dea88f
lashagelashvili <las.gelashvili@gmail.com>
2021-07-24 03:06:52 +0400

    deleted unnecessary lines

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,11 +1,11 @@
 export interface Question {
+    id: number;
     category: string;
     type: string;
     difficulty: string;
     question: string;
     correct_answer: string;
     incorrect_answers: string[];
-    pick_one: string[];
 }
 
 export interface Quizz {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-// import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatRadioModule} from '@angular/material/radio';
-import {HttpClientModule} from '@angular/common/http';
-
-
-
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { ResultComponent } from './result/result.component';
 import { SelectorComponent } from './selector/selector.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
@@ -35,11 +32,13 @@ import { SelectorComponent } from './selector/selector.component';
     BrowserAnimationsModule,
     MatSelectModule,
     BrowserAnimationsModule,
-    // MatCheckboxModule,
     MatButtonModule,
     MatDividerModule,
     MatRadioModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatCardModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
