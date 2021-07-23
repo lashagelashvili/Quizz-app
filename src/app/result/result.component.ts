61cf4ce4060a362b04cf4a589ae478623f794948
lashagelashvili <las.gelashvili@gmail.com>
2021-07-24 03:07:20 +0400

    deleted unnecessary lines

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StateService } from '../services/state.service';
 
 @Component({
   selector: 'app-result',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent implements OnInit {
+  message: number;
 
-  constructor() { }
+  constructor(private shared: StateService) { }
 
   ngOnInit(): void {
+    this.message = this.shared.getMessage()
   }
-
 }
